9f70df3482f72e66cfcee34cc6fa1c16435ee21f
jeffmsmithdev@gmail.com <jeffmsmithdev@gmail.com>
2012-12-13 10:37:58 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Knocta::Application.config.secret_token = '0b685a13c0339e3bec31eb2456e36b1fa44d1e264a1ca58edd1ce68603752d035dced448bb4183a5deaf5f20a23083adeb5c56d50793f0b42084d1b92f0f9c5d'
